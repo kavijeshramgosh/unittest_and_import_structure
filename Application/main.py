16d4d90086a1abe4fe24f411165f5075aaf080eb
kavijeshramgosh <kavijeshramgosh@gmail.com>
2024-11-09 14:02:45 +0000

    Working main.py

--- Application/main.py
+++ Application/main.py
@@ -1,10 +1,13 @@
 #Importing First_script and Two_func() using __init__.py
-from Application import *
+
+import First_script #Importing whole of "First.py"
+from Second_script import * #Importing all functions from "second.py"
 
 First_script.One_Func() #Can call any function in First through First.
 Two_Func() #Calling Two directly from Second (I think this lacks clarity of where it has come from)
 
-'''
+
+
 
 #Importing from a script in a different directory (using __init__.py)
 from package import * #Importing using init file in packages
@@ -14,12 +17,9 @@ Four_Func() #Only Four_Func is imported in __init__.py file in "package" folder
 
 
 
+
 #Importing from a script in a different subdirectory (using __init__.py in main package)
 from package.subpackage import * #Importing using init file in subpackages 
 
 Fifth_script.Five_Func() #Imports all of "Fifth_script.py" but runs just Five_Func
 Six_Func() #Only Six_Func imported through __init__.py file in "subpackages" folder  
-'''
-
-if __name__ == "__main__":
-    main()
